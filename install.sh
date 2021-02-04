742f3bba03f1a2c1cb614fd29ba6e551463b4fc9
flashlan <flash_lan@hotmail.com>
2021-02-04 01:49:53 -0300

    update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,47 @@
 #!/bin/sh
 
-cp iptablesOTF.sh /usr/loca/bin/
+cp iptablesOTF.sh /usr/local/bin/iptablesOTF.sh
 
-echo "Script is executed as : `whoami`"
+echo "Script is running as : `whoami`"
 
 var0=$(who am i | awk '{print $1}')
-echo "var0 is $var0"
+echo "Will be instaled to user $var0"
 
 ##################################################
-cd /home/$var0/.iptablesOTF
 
-# create folder e environment
-mkdir /home/$var0/.iptablesOTF
-mkdir /home/$var0/.iptablesOTF/backups
-touch /home/$var0/.iptablesOTF/iptables.conf
+
+# create folder iptables 
+if [ ! -e /home/$var0/.iptablesOTF ]; then # -e 
+    mkdir /home/$var0/.iptablesOTF
+    echo "folder /home/$var0/.iptablesOTF created"
+    else 
+        echo "folder /home/$var0/.iptablesOTF exist! Skiping!"
+    fi
+# create backup folder 
+if [ ! -e /home/$var0/.iptablesOTF/backups ]; then
+    mkdir /home/$var0/.iptablesOTF/backups
+    echo "folder /home/$var0/.iptablesOTF/backups created"
+    else 
+        echo "folder /home/$var0/.iptablesOTF/backups exist! Skiping!"
+    fi
+# create empty iptables file folder 
+if [ ! -f /home/$var0/.iptablesOTF/iptables.conf ]; then
+    touch /home/$var0/.iptablesOTF/iptables.conf
+fi
+
+#cd /home/$var0/.iptablesOTF
 cp iptables.conf_dummy_workstation /home/$var0/.iptablesOTF/
-cp iptables.conf_dummy_workstation /home/$var0/.iptablesOTF/
+cp iptables.conf_dummy_server /home/$var0/.iptablesOTF/
+
+#adjust permissions
+cd /home/$var0 #/.iptablesOTF/backups
+chown $var0 .iptablesOTF
+cd /home/$var0/.iptablesOTF 
+chown $var0 backups
+find . -type f -name '*.conf' | xargs chown $var0
+#cd ..
+chown $var0 *
+
+echo "Done! "
+echo "execute iptables.sh to configure and backup iptables rules"
+echo "backups are in /home/$var0/.iptablesOTF/backups"
--- iptablesOTF.sh
+++ iptablesOTF.sh
@@ -2,51 +2,21 @@
 
 echo "Script is executed as : `whoami`"
 
-
 var0=$(who am i | awk '{print $1}')
 echo "var0 is $var0"
 
-
-#var1=$var0
-#echo "var1 is $var1"
-
-#var3=$(getent passwd var0 | cut -d: -f6)
-#echo "var3 is $var3"
-
-
-#echo "Switching user to $different_user"
-# Switching user to deploy
-#echo ~var1
-#echo "$userRoot"
-
-#su -l -p root
-#echo "Current user: `whoami`"
-# Current user: root
-#echo "Current user: `id`"
-
-###############################################################
-cd /home/$var0/.firewall
-
-
-
-# create folder e environment
-mkdir /home/$var0/.firewall
-mkdir /home/$var0/.firewall/backups
-cp /home/$var0/.firewall/iptables.conf /home/$var0/.firewall/backups/"iptables_orig-`date`.txt"
-touch /home/$var0/.firewall/iptables.conf
-#basedir=$(/home/$var0/.firewall $0)
-
+##################################################
+cd /home/$var0/.iptablesOTF
+cp /home/$var0/.iptablesOTF/iptables.conf /home/$var0/.iptablesOTF/backups/"iptables_orig-`date`.txt"
 #debug 
 echo "backup created  in /home/$var0/backups "
 
 # backup
-cp /var/lib/iptables/rules-save /home/$var0/.firewall/backups/"rules-save_orig-`date`.txt"
-cd /home/$var0/.firewall/backups
+cp /var/lib/iptables/rules-save /home/$var0/.iptablesOTF/backups/"rules-save_orig-`date`.txt"
+cd /home/$var0/.iptablesOTF/backups
 chown orfeu *
 cd ..
 #chwon orfeu backups/*.*
-
-
 #echo " backup criado: var is /home/$var0 "
 
 # stop service
@@ -54,24 +24,24 @@ rc-service iptables stop
 rc-service ip6tables stop
 
 rm /var/lib/iptables/rules-save 
-chmod +x /home/$var0/.firewall/iptables.conf 
+chmod +x /home/$var0/.iptablesOTF/iptables.conf 
 
 echo "old rules are saved  /home/$var0/backups "
 
 # open new config to editing
-kate /home/$var0/.firewall/iptables.conf 
+kate /home/$var0/.iptablesOTF/iptables.conf 
 echo "____________Done Editing!_____________"
 
 # executa configuracao
-/home/$var0/.firewall/iptables.conf
+/home/$var0/.iptablesOTF/iptables.conf
 
 # update services
 /etc/init.d/iptables save 
 /etc/init.d/ip6tables save
 
 # salva backup do arquivo alterado
-cp /var/lib/iptables/rules-save /home/$var0/.firewall/backups/"rules-save_editado`date`.txt"
-cd /home/$var0/.firewall/backups
+cp /var/lib/iptables/rules-save /home/$var0/.iptablesOTF/backups/"rules-save_editado`date`.txt"
+cd /home/$var0/.iptablesOTF/backups
 chown orfeu *
 cd ..
 
@@ -86,5 +56,5 @@ echo ""
 echo "_____________DONE!_________________"
 echo ""
 echo ""
-echo "All config is saved in /home/$var0/.firewall dir"
-echo "All backups are located at /home/$var0/.firewall/backups dir"
+echo "All config is saved in /home/$var0/.iptablesOTF dir"
+echo "All backups are located at /home/$var0/.iptablesOTF/backups dir"
